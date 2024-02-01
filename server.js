0eead67b5d0ebef5439c6c6da2a82258260ef57a
Philippe Neri Singizwa <neriwest20@gmail.com>
2024-02-01 09:19:46 -0500

    persist user sessions

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios"
+import session from "express-session";
+import passport from "passport";
+import { Strategy } from "passport-local";
+
 
 
 const app = express();
@@ -9,62 +13,106 @@ const API_URL = "http://localhost:4000"
 let msg;
 let isAuthenticated = false;
 let isNewUser = false;
-const saltRounds = 10;
 
-app.use(express.static("public"));
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
+app.use(session({
+    secret: "KEYTOBEUSEDTOSTORETHESESSION",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 2,
+    }
+  }));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/",(req, res)=>{
-    res.render("register.ejs");
+    if(req.isAuthenticated()){
+        res.redirect("/content")
+    }else{
+        res.render("register.ejs");
+    }
 })
 
 app.get("/login",(req, res)=>{
     res.render("login.ejs");
 });
+app.get("/content", (req, res)=>{
+    // console.log(req.user)
+    if(req.isAuthenticated()){
+        if(req.user.length===0){
+            res.render("content.ejs", {content: req.user});
+        }else{
+            res.render("content.ejs", {content: JSON.parse(req.user)});
+        }
+        
+    } else{
+        res.redirect("/login")
+    }
+});
 
 app.get("/add", (req, res)=>{
-    if(!isAuthenticated){
-        res.redirect("/login")
-    } else{
+    if(req.isAuthenticated()){
         res.render("editForm.ejs")
+    } else{
+        res.redirect("/login")
     }
 });
 app.post("/register", async (req, res)=>{
-    const {email, password} = req.body;
+    const {username, password} = req.body;
     
     try {
-        const response = await axios.post(`${API_URL}/register`, {email: email, password: password});
-        if (response.data.content.length !== 0){
-            res.render("content.ejs", {content: JSON.parse(response.data.content)});
-        } else{
-            res.render("content.ejs");
-        }
+        const response = await axios.post(`${API_URL}/register`, {email: username, password: password});
+        const user = response.data.content;
+        req.login(user, (err)=> {
+            if(err) console.log(err.message);
+            res.redirect("/content");
+        });
         isAuthenticated = true;
     } catch (error) {
         res.redirect('/');
-        console.error(error.message);
+        console.error("register: " +error.message);
     }
 
     
 });
 
-app.post("/login", async (req, res)=>{
-    const {email, password} = req.body;
+app.post("/login", passport.authenticate("local",{
+    successRedirect: "/content",
+    failureRedirect: "/login",
+}));
+
+passport.use(new Strategy(async function verify(username, password, cb){
     try{
-        const response = await axios.post(`${API_URL}/login`, {email: email, password: password});
+        const response = await axios.post(`${API_URL}/login`, {email: username, password: password});
+        const user = response.data.content;
         if (response.data.status){
             isAuthenticated = true;
-            res.render("content.ejs", {content: JSON.parse(response.data.content)});
+            
+            return cb(null, user);
+            // res.render("content.ejs", {content: JSON.parse(response.data.content)});
         } else{
-            res.redirect("/login");
+            // res.redirect("/login");
+            console.log("user not found")
+            return cb(null, false);
         }
         
     } catch (error) {
-        res.redirect("/login");
-        console.error(error.message)
+        console.log(error.message);
+        // res.redirect("/login");
+        return cb(error.message);
     }
+}));
 
+passport.serializeUser((user, cb)=>{
+    cb(null, user);
+});
+passport.deserializeUser((user, cb)=>{
+    cb(null, user);
 });
 
 app.post("/add", async (req, res)=>{
@@ -97,6 +145,9 @@ app.post("/edit", async (req, res)=>{
     res.render("content.ejs", {content: JSON.parse(response.data.content)});
     console.log(response.data);
 });
+
+
+
 
 
 app.listen(port, ()=>{
